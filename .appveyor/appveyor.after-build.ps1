b4488d34d53ea1f916596f307a2cb5ccf7ac1e53
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-14 01:08:56 +0200

    mmmmf

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -7,13 +7,13 @@ robocopy $path\lowcdc\Release $path\deploy *.sys /s ;
 # Copy particle.inf into deploy folder
 Copy-Item $path\particle.inf $path\deploy\particle.inf ;
 
-iex "`"${env:INF2CAT}`" /v /driver:$path\deploy /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,6_3_X86,6_3_X64,6_3_ARM";
+cmd.exe /c "`"${env:INF2CAT}`" /v /driver:$path\deploy /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,6_3_X86,6_3_X64,6_3_ARM";
 
 $sign = "`"${env:SIGNTOOL}`"  sign /v /f windows_key.p12 /p %key_secret% /tr http://tsa.starfieldtech.com";
 
-iex $sign $path\deploy\x86\lowcdc.sys ;
-iex $sign $path\deploy\amd64\lowcdc.sys ;
-iex $sign $path\deploy\particle.cat ;
+cmd.exe /c "$sign $path\deploy\x86\lowcdc.sys" ;
+cmd.exe /c "$sign $path\deploy\amd64\lowcdc.sys" ;
+cmd.exe /c "$sign $path\deploy\particle.cat" ;
 
 # Create a zip
 7z a windows-device-drivers.zip $path\deploy\* ;
