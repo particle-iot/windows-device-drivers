8bce232b4a35092e15d5b685c964df1f8dda3e21
Andrey Tolstoy <avtolstoy@gmail.com>
2016-06-14 19:21:02 +0700

    Added AddTrust cross-certificate (#1)

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -9,7 +9,7 @@ Copy-Item $path\particle.inf $path\deploy\particle.inf ;
 
 cmd.exe /c "`"${env:INF2CAT}`" /v /driver:$path\deploy /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,6_3_X86,6_3_X64,6_3_ARM";
 
-$sign = "`"${env:SIGNTOOL}`"  sign /v /f windows_key.p12 /p %key_secret% /tr http://tsa.starfieldtech.com";
+$sign = "`"${env:SIGNTOOL}`"  sign /v /ac AddTrust_External_CA_Root.cer /f windows_key.p12 /p %key_secret% /tr http://tsa.starfieldtech.com";
 
 cmd.exe /c "$sign $path\deploy\x86\lowcdc.sys" ;
 cmd.exe /c "$sign $path\deploy\amd64\lowcdc.sys" ;
