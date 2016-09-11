5f2d4fcae372b080ea56aa9bf3efa46b4b9db05b
Andrey Tolstoy <andrey@enistek.com>
2016-09-11 09:01:27 -0700

    Add drivers dir to makensis params

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -22,6 +22,7 @@ cmd.exe /c "$sign $path\deploy\particle.cat" ;
 7z -y -o"${env:NSIS}" x $path\installer\plugins\ExecDos.zip "Plugins";
 
 # Create an installer
+$param = "/DPRODUCT_VERSION=${env:APPVEYOR_BUILD_VERSION}", "/DDRIVERSDIR=$path\deploy", "$path\installer\installer.nsi" ;
 & "${env:MAKENSIS}" $param ;
 # Sign
 cmd.exe /c "$sign $path\installer\particle_drivers_${env:APPVEYOR_BUILD_VERSION}.exe" ;
